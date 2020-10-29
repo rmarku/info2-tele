27fff066d39231cdc45eb5052476f2072c706dfd
Ricardo Martín Marcucci <rmarku@gmail.com>
2020-10-29 13:18:08 -0300

    Agregada entidad como estructura para raylib

--- ej_raylib/mainRaylib.c
+++ ej_raylib/mainRaylib.c
@@ -1,44 +1,71 @@
 #include "raylib.h"
 
+typedef struct {
+    float velocidad;
+    Texture2D texture;
+    Vector2 pos;
+} Entidad;
+
+void DrawEntidad(Entidad ent) {
+    DrawTextureV(ent.texture, ent.pos, WHITE);
+}
+
+void MoverEntidad(Entidad *ent) {
+    ent->pos.x += 0.2;
+}
+
+
 int main(void) {
     const int screenWidth = 512;
     const int screenHeight = 366;
 
     const float gravity = 0.5;
-    float vel = 0;
-    float fondoX = 0;
 
     InitWindow(screenWidth, screenHeight, "Info II - Tele");
-    Texture2D nave = LoadTexture("nave.png");        // Texture loading
+    Entidad nave;
+    Entidad enemigos[5];
+
+    for (int i = 0; i < 5; ++i) {
+        enemigos[i].texture = LoadTexture("nave.png");        // Texture loading
+        enemigos[i].pos.x = 33 * i;
+        enemigos[i].pos.y = 33 * i;
+        enemigos[i].velocidad = 0;
+    }
+
+    nave.texture = LoadTexture("nave.png");        // Texture loading
+    nave.pos.x = (float) screenWidth / 2 - nave.texture.width / 2;
+    nave.pos.y = (float) screenHeight / 2 - nave.texture.height / 2;
+    nave.velocidad = 0;
+
+    float fondoX = 0;
     Texture2D fondo = LoadTexture("fondo.png");        // Texture loading
 
-    Vector2 navePos = {
-            (float) screenWidth / 2 - nave.width / 2,
-            (float) screenHeight / 2 - nave.height / 2
-    };
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
 
         if (IsKeyDown(KEY_UP))
-            vel = -4.0f;
+            nave.velocidad = -4.0f;
         if (IsKeyDown(KEY_RIGHT))
-            navePos.x += 2.0f;
+            nave.pos.x += 2.0f;
         if (IsKeyDown(KEY_LEFT))
-            navePos.x -= 2.0f;
+            nave.pos.x -= 2.0f;
 
-        vel = vel + gravity;
-        navePos.y = navePos.y + vel;
+        nave.velocidad = nave.velocidad + gravity;
+        nave.pos.y = nave.pos.y + nave.velocidad;
 
-        if (navePos.y >= screenHeight) {
-            vel = -vel;
-            navePos.y = screenHeight;
+        if (nave.pos.y >= screenHeight) {
+            nave.velocidad = -nave.velocidad;
+            nave.pos.y = screenHeight;
         }
 
         fondoX -= 1.5;
         if (fondoX < -fondo.width) {
             fondoX += fondo.width;
+        }
+        for (int i = 0; i < 5; ++i) {
+            MoverEntidad(&enemigos[i]);
         }
 
 
@@ -46,7 +73,13 @@ int main(void) {
         ClearBackground(RAYWHITE);
         DrawTexture(fondo, fondoX, 0, WHITE);
         DrawTexture(fondo, fondo.width + fondoX, 0, WHITE);
-        DrawTexture(nave, navePos.x, navePos.y, WHITE);
+
+
+        DrawEntidad(nave);
+        for (int i = 0; i < 5; ++i) {
+            DrawEntidad(enemigos[i]);
+        }
+
         DrawText("Nave", 10, 10, 20, DARKGRAY);
         EndDrawing();
 
